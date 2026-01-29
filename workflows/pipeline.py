9a7dcb3bb567853cb7a727618a70f7f4b88fcfcb
nuuuwan <nuuuwan@gmail.com>
2026-01-29 09:06:37 +0530

    Fix argument parser to correctly handle required directory path

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -5,7 +5,8 @@ from jp import JournalPaper
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build a journal paper")
     parser.add_argument(
-        "dir_path", help="Path to the paper directory", required=True
+        "dir_path",
+        help="Path to the paper directory",
     )
     args = parser.parse_args()
 
