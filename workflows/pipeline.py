8b6e7e38792cb07e65c8c8aba12964abf0c7a056
nuuuwan <nuuuwan@gmail.com>
2026-01-29 09:05:11 +0530

    Refactor pipeline to accept directory path as a command-line argument

--- workflows/pipeline.py
+++ workflows/pipeline.py
@@ -1,5 +1,13 @@
+import argparse
+
 from jp.core.JournalPaper import JournalPaper
 
 if __name__ == "__main__":
-    paper = JournalPaper("private/func")
+    parser = argparse.ArgumentParser(description="Build a journal paper")
+    parser.add_argument(
+        "dir_path", help="Path to the paper directory", required=True
+    )
+    args = parser.parse_args()
+
+    paper = JournalPaper(args.dir_path)
     paper.build()
